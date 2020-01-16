a7dd7285dc58835d8a07dd0cfc1a381786b7a232
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-16 22:27:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,6 +6,10 @@ def map_to_no_change(source_array)
   source_array
 end
 
-def map_to_negativize(source_array)
+def map_to_double(source_array)
   source_array *= 2
+end
+
+def map_to_square(source_array)
+  source_array **= 2
 end
