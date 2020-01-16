9e624ad767ab52fcdc166976b98b9b06bb681777
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-16 22:44:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,7 +30,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  running_total = 0
+  running_total = 10
   while starting_point < source_array.size 
     running total += source_array[starting_point]
     starting_point += 1 
